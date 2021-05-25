4c3c1e88268166ffa48b337b2356dca101d9594f
minseonyu <msyu@minseonyuui-MacBookPro.local>
2021-05-25 11:11:45 +0900

    setting log

--- src/main/kotlin/com/day1/callback/service/imp/ImpService.kt
+++ src/main/kotlin/com/day1/callback/service/imp/ImpService.kt
@@ -3,7 +3,10 @@ package com.day1.callback.service.imp
 import com.day1.callback.web.dto.ImpRequestDto
 import com.day1.callback.web.dto.ImpResponseDto
 import mu.KotlinLogging
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
+import org.springframework.web.client.RestTemplate
+import org.springframework.web.client.postForEntity
 
 private val logger = KotlinLogging.logger {}
 
@@ -11,15 +14,24 @@ private val logger = KotlinLogging.logger {}
 class ImpService {
 
     fun callbackData(dto: ImpRequestDto): ImpResponseDto {
-        logger.debug("service")
+        logger.info { "service" }
 
         //ip Check
-
+        var check = callbackDay1(dto)
         //local env Check
-
+        logger.info { "check $check" }
         dto.ip = 127
         dto.method = "json"
 
         return ImpResponseDto(dto.ip, dto.method)
+    }
+
+    fun callbackDay1(impRequestDto: ImpRequestDto): String {
+
+        val entity: ResponseEntity<String> = RestTemplate().postForEntity(
+            "http://192.168.5.102:3001/imp.do",impRequestDto)
+
+        logger.info { "entity data $entity" }
+        return "true"
     }
 }
--- src/main/kotlin/com/day1/callback/web/ImpApiController.kt
+++ src/main/kotlin/com/day1/callback/web/ImpApiController.kt
@@ -4,12 +4,14 @@ import com.day1.callback.service.imp.ImpService
 import com.day1.callback.web.dto.ImpRequestDto
 import com.day1.callback.web.dto.ImpResponseDto
 import lombok.RequiredArgsConstructor
+import mu.KLogging
 import org.springframework.web.bind.annotation.*
 
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/pg")
 class ImpApiController(val impService: ImpService) {
+    companion object: KLogging()
 
     @GetMapping("/")
     fun index(): String {
