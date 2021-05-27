153630272dd914e87046ddb06676bf961d2344f1
alstjs1207 <alstjs1207@naver.com>
2021-05-27 10:22:39 +0900

    add config

--- src/main/kotlin/com/day1/callback/service/imp/ImpService.kt
+++ src/main/kotlin/com/day1/callback/service/imp/ImpService.kt
@@ -1,19 +1,16 @@
 package com.day1.callback.service.imp
 
+import com.day1.callback.config.ImpConfigProperties
 import com.day1.callback.web.dto.ImpRequestDto
 import com.day1.callback.web.dto.ImpResponseDto
 import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
 import mu.KotlinLogging
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.MediaType
 import org.springframework.http.RequestEntity
 import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Service
 import org.springframework.web.client.RestTemplate
 import org.springframework.web.client.exchange
-import org.springframework.web.client.postForEntity
 
 private val logger = KotlinLogging.logger {}
 
@@ -22,7 +19,8 @@ fun Any.convert2ObjectString(): String = mapper.writeValueAsString(this)
 fun ResponseEntity<HashMap<String, Any>>.convert2MapBody(): Any? = this.body?.get("body")
 
 @Service
-class ImpService {
+class ImpService (private val impConfigProperties: ImpConfigProperties){
+
     fun callbackData(dto: ImpRequestDto) {
         logger.info { "service" }
         //ip Check
@@ -30,29 +28,19 @@ class ImpService {
         //callback day1
         var resDto = callbackDay1(dto)
         logger.info { "check $resDto" }
+
     }
 
     fun callbackDay1(impRequestDto: ImpRequestDto): ImpResponseDto {
         //val restTemplate: RestTemplate()
         val entity : ResponseEntity<HashMap<String, Any>> = RestTemplate().exchange(
-            RequestEntity.post("http://localhost:8002/pg/imp")
+            RequestEntity.post(impConfigProperties.endpoint)
                 .contentType(MediaType.APPLICATION_JSON)
                 .body(impRequestDto)
         )
 
         logger.info { "entity data $entity" }
-        val result = ObjectMapper().readValue<ImpResponseDto>(entity.convert2MapBody()?.convert2ObjectString(),ImpResponseDto::class.java)
-        //성공
-        if(result.imp_success) {
-            //이력
-            logger.info { "CALLBACK SUCCESS!!" }
-        }
-        //실패
-        else {
-            //이력
-            logger.error { "CALLBACK FAIL!" }
-        }
 
-        return result
+        return ObjectMapper().readValue<ImpResponseDto>(entity.convert2MapBody()?.convert2ObjectString(),ImpResponseDto::class.java)
     }
 }
--- src/main/kotlin/com/day1/callback/web/dto/ImpResponseDto.kt
+++ src/main/kotlin/com/day1/callback/web/dto/ImpResponseDto.kt
@@ -3,6 +3,5 @@ package com.day1.callback.web.dto
 data class ImpResponseDto (
     var imp_uid: String = "",
     var merchant_uid: String = "",
-    var status: String = "",
-    var imp_success: Boolean = false
+    var status: String = ""
     )
