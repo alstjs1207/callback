c2b74614380e4896f50e8b83cb63a582b4f2a062
alstjs1207 <alstjs1207@naver.com>
2021-05-31 17:13:35 +0900

    api request 수정

--- src/test/kotlin/com/day1/callback/web/HttpApiTests.kt
+++ src/test/kotlin/com/day1/callback/web/HttpApiTests.kt
@@ -19,7 +19,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension
 // top-level Functions
 val mapper = ObjectMapper()
 fun Any.convert2ObjectString(): String = mapper.writeValueAsString(this)
-fun ResponseEntity<HashMap<String, Any>>.convert2MapBody(): Any? = this.body?.get("body")
+fun ResponseEntity<HashMap<String, Any>>.convert2MapBody(): Any? = this.body?.get("data")
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS) //BeforeAll, AfterAll를 위해 사용
 @ExtendWith(SpringExtension::class)
@@ -36,15 +36,12 @@ class HttpApiTests @Autowired constructor(
     fun `call node iamport`() {
         //given
         var impRequestDto = ImpRequestDto("imp_1234567890", "merchant_1234567890", "ready")
-        //var impRequestDto = ImpRequestDto("imp_910037693783", "ptx-fc-91-4-20210526021037203", status = "paid")
         // api - callback 호출
-        //val url ="https://api.local.fastcampus.co.kr/callback/pg/imp"
-        // 기존 callback 호출
-        val url2 ="http://localhost:8002/pg/imp"
+        val url ="https://api.local.fastcampus.co.kr/callback/pg/imp"
 
         //when
         val entity : ResponseEntity<HashMap<String, Any>> = restTemplate.exchange(
-            RequestEntity.post(url2)
+            RequestEntity.post(url)
                 //.header("Authorization","bearer 21fc92a79da2b6686a735e6c3aab60b0bbc4")
                 //.header("User-Agent","Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36")
                 .contentType(MediaType.APPLICATION_JSON)
@@ -57,6 +54,8 @@ class HttpApiTests @Autowired constructor(
         Assertions.assertThat(entity.statusCode).isEqualTo(HttpStatus.ACCEPTED) //202
         Assertions.assertThat(result.imp_uid).isEqualTo(impRequestDto.imp_uid)
         Assertions.assertThat(result.merchant_uid).isEqualTo(impRequestDto.merchant_uid)
+        Assertions.assertThat(result.status).isEqualTo(impRequestDto.status)
+        Assertions.assertThat(result.imp_success).isEqualTo(true)
 
     }
 
