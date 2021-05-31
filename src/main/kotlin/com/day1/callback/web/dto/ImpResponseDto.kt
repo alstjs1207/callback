fc61fcb514a8f9a14fc5e12408bb561c942bf21c
alstjs1207 <alstjs1207@naver.com>
2021-05-31 17:13:35 +0900

    api request 수정

--- src/main/kotlin/com/day1/callback/web/dto/ImpResponseDto.kt
+++ src/main/kotlin/com/day1/callback/web/dto/ImpResponseDto.kt
@@ -3,5 +3,6 @@ package com.day1.callback.web.dto
 data class ImpResponseDto (
     var imp_uid: String = "",
     var merchant_uid: String = "",
-    var status: String = ""
+    var status: String = "",
+    var imp_success: Boolean = false
     )
