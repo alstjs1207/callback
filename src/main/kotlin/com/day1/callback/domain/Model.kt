a111c54d744df55fdd08691f680da0853e4268d0
alstjs1207 <alstjs1207@naver.com>
2021-05-31 17:13:35 +0900

    api request 수정

--- src/main/kotlin/com/day1/callback/domain/Model.kt
+++
@@ -1,9 +0,0 @@
-package com.day1.callback.domain
-
-data class Imp (
-    val imp_uid: String,
-    val merchant_uid: String,
-    val status: String)
-
-data class Kakao (
-    val id: String)
--- src/main/kotlin/com/day1/callback/service/imp/ImpService.kt
+++ src/main/kotlin/com/day1/callback/service/imp/ImpService.kt
@@ -16,18 +16,19 @@ private val logger = KotlinLogging.logger {}
 
 val mapper = ObjectMapper()
 fun Any.convert2ObjectString(): String = mapper.writeValueAsString(this)
-fun ResponseEntity<HashMap<String, Any>>.convert2MapBody(): Any? = this.body?.get("body")
+fun ResponseEntity<HashMap<String, Any>>.convert2MapBody(): Any? = this.body?.get("data")
 
 /**
  * 결제 서비스
  */
 
 @Service
-class ImpService (private val impConfigProperties: ImpConfigProperties){
+class ImpService (private val impConfigProperties: ImpConfigProperties): ImpServiceImpl {
 
     fun callbackData(dto: ImpRequestDto) {
         logger.info { "service" }
         //ip Check todo Spring Security
+        //52.78.100.19, 52.78.48.223
 
         //callback day1
         var resDto = callbackDay1(dto)
@@ -35,7 +36,7 @@ class ImpService (private val impConfigProperties: ImpConfigProperties){
 
     }
 
-    fun callbackDay1(impRequestDto: ImpRequestDto): ImpResponseDto {
+    override fun callbackDay1(impRequestDto: ImpRequestDto): ImpResponseDto {
 
         val entity : ResponseEntity<HashMap<String, Any>> = RestTemplate().exchange(
             RequestEntity.post(impConfigProperties.endpoint)
