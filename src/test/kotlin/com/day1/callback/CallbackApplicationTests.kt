a5b1a1336ec9bbb9c4c25d7f79e55e92cc48ccce
alstjs1207 <alstjs1207@naver.com>
2021-08-30 02:21:30 +0900

    add test code main profiles

--- src/test/kotlin/com/day1/callback/CallbackApplicationTests.kt
+++ src/test/kotlin/com/day1/callback/CallbackApplicationTests.kt
@@ -3,8 +3,10 @@ package com.day1.callback
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.ActiveProfiles
 import org.springframework.test.context.junit.jupiter.SpringExtension
 
+@ActiveProfiles("local")
 @ExtendWith(SpringExtension::class)
 @SpringBootTest
 class CallbackApplicationTests {
